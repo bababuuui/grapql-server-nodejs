e930583a524b1163ddd10c728de3a6f66adef8c9
bababuuui <bubinatar@gmail.com>
2022-07-07 17:17:00 +0200

    docs - add readme

--- src/app/index.ts
+++ src/app/index.ts
@@ -12,10 +12,6 @@ import "dotenv/config";
 
 console.log(`start`);
 
-// TODO eslint errors
-// TODO chains? ? ? ?
-// todo readme
-
 const server = new ApolloServer({
   typeDefs: TYPE_DEFINITIONS,
   resolvers: RESOLVERS,
@@ -33,7 +29,6 @@ const server = new ApolloServer({
   context: (expressContext) => {
     return {
       token: expressContext.req.headers.authorization || "default_token",
-      //  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmI5ZTEwNGJkZWU0NzFjMzM1OWI5ZWMiLCJmaXJzdE5hbWUiOiJmaXJzdCBuYW1lIiwibGFzdE5hbWUiOiJsYXN0IG5hbWUiLCJlbWFpbCI6Im1ldDkxMjlAZ21haWwuY29tIiwiaWF0IjoxNjU2NDQzMzUxfQ.UpIpK6D3qQGejD3oWWGIQ0Y-oI94ev8T6EO0qWH1XhU",
     };
   },
   csrfPrevention: true,
